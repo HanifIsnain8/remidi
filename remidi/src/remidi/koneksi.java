d762276655a7fb7e804b231d8aeeebb819faabdf
Hanif Isnain <117495061+HanifIsnain8@users.noreply.github.com>
2023-11-21 10:23:57 +0700

    Add files via upload

---
+++ remidi/src/remidi/koneksi.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package remidi;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hanif
+ */
+public class koneksi {
+    public static Connection sambung_ke_db(){
+        try{
+            MysqlDataSource m = new MysqlDataSource();
+            m.setUser("root");
+            m.setPassword("");
+            m.setDatabaseName("mahasiswa");
+            m.setServerName("Localhost");
+            m.setPortNumber(3306);
+            m.setServerTimezone("Asia/Jakarta");
+        
+            Connection c = m.getConnection();
+            System.out.println("Sukses Terhubung ke database");
+            return c;
+        } catch (SQLException e){
+            System.err.println("Gagal Terhubung ke database");
+            System.err.println("error"+e.getMessage());
+        }
+        return null;
+    }
+    
+}
